153ffd711fb87a7e3a3eddae14c0a04681c058c6
WYZ00 <thanthtettin@gmail.com>
2023-10-29 22:05:56 +0630

    chapter reorder functionality

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -4,7 +4,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
 
 import { useRouter } from "next/navigation";
@@ -21,6 +21,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { toast } from "@/components/ui/use-toast";
 import { Input } from "@/components/ui/input";
+import { ChaptersList } from "./chapters-list";
 
 interface ChaptersFormProps {
   initialData: Course & { chapters: Chapter[] };
@@ -64,8 +65,33 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
     }
   };
 
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+      toast({
+        title: "Chapter reordered",
+      });
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: "Something went wrong",
+        variant: "destructive",
+      });
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course Chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -115,7 +141,11 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
             !initialData.chapters.length && "text-slate-500 italic"
           )}>
           {!initialData.chapters.length && "No chapters"}
-          {/* {Todo:Add a list of chapters} */}
+          <ChaptersList
+            onEdit={() => {}}
+            onReOrder={onReorder}
+            items={initialData.chapters || {}}
+          />
         </div>
       )}
       {!isCreating && (
