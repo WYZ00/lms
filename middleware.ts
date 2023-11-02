3e63b95d368acdd8d80e86b6eb2060c368ef8ab4
WYZ00 <thanthtettin@gmail.com>
2023-11-02 22:25:27 +0630

    course publish action and refactoring

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   // publicRoutes: ["/test"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/uploadthing"],
 });
 
 export const config = {
